9d868758596ab9cb493fb396f4ba28156b6d01f2
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-09-25 22:21:49 +0300

    refactor: add task description

--- Lab1/src/Task1.java
+++ Lab1/src/Task1.java
@@ -1,5 +1,11 @@
 public class Task1 {
 
+    /*
+    *  В переменных х и y хранятся два натуральных числа. Создайте программу, выводящую на консоль:
+    *   результат целочисленного деления x на y;
+    *   остаток от деления x на y;
+    *   квадратный корень x.
+     */
     public static void main(String[] args) {
         GetOperations();
     }
@@ -8,12 +14,14 @@ public class Task1 {
         int x = 9;
         int y = 3;
 
-        int integerDivision = x/y;
-        int remainderDivision = x%y;
+        int integerDivision = x / y;
+        int remainderDivision = x % y;
         double squareRoot = Math.sqrt(x);
 
-        System.out.println(String.format("Целочисленное деление %d, остаток от деления %d, квадратный корень %f", integerDivision, remainderDivision, squareRoot));
+        System.out.println(String.format("""
+                Целочисленное деление %d,
+                остаток от деления %d,
+                квадратный корень %f""",
+                integerDivision, remainderDivision, squareRoot));
     }
-
-
 }
