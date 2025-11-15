8b420e50aba8b7f725f515f640be55b6d473cf38
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-11-15 22:07:45 +0300

    feat: add completed Task1

---
+++ Lab7/src/Task01.java
@@ -0,0 +1,35 @@
+
+/*
+* Создать два конструктора.
+•	С параметром типа int. Задающего начальную емкость массива. Принимает один параметр (задает capacity),
+* выделяет память под массив (size = 0).
+•	По умолчанию (без параметров). Который выделяет память под массив на 10 элементов,
+*  равных нулю (capacity = 10, size = 0). Переиспользовать конструктор с параметрами для уменьшения кода.
+
+
+* */
+public class Task01 {
+    public static void main(String[] args) {
+
+        // Конструктор по умолчанию
+        MyArrayList<String> defaultList = new MyArrayList<>();
+        System.out.println("Конструктор по умолчанию: size = " + defaultList.getSize());
+
+        // Конструктор с параметром
+        MyArrayList<Integer> customList = new MyArrayList<>(5);
+        System.out.println("Конструктор с емкостью 5: size = " + customList.getSize());
+
+        // Демонстрация с разными типами
+        MyArrayList<Double> doubleList = new MyArrayList<>(3);
+        System.out.println("Double список создан: size = " + doubleList.getSize());
+
+        // Обработка исключений
+        try {
+            MyArrayList<Object> invalidList = new MyArrayList<>(-1);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Перехвачено исключение: " + e.getMessage());
+        }
+
+        System.out.println("Все конструкторы работают корректно!");
+    }
+}
