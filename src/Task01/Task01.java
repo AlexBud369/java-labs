d846f9a76403f29abfac324a79d550aecb8fd6c0
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-12-04 08:40:02 +0300

    refactor: add tasks text

--- src/Task01/Task01.java
+++ src/Task01/Task01.java
@@ -1,5 +1,8 @@
 package Task01;
 
+/*Написать программу «Калькулятор», в котором все действия сохраняются в файл.
+ Программа должна предоставлять возможность просмотра истории операций.*/
+
 public class Task01 {
     public static void main(String[] args) {
         CalculatorApp app = new CalculatorApp();
