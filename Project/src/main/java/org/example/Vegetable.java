cba46bc3842aabaaf65f6e4ded25ce352d82a324
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-12-25 19:57:09 +0300

    feat: add Vegetable.java file

---
+++ Project/src/main/java/org/example/Vegetable.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Vegetable {
+    private int id;
+    private String name;
+    private int calories;
+
+    public Vegetable(int id, String name, int calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+    // Геттеры и сеттеры
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getCalories() { return calories; }
+    public void setCalories(int calories) { this.calories = calories; }
+
+    @Override
+    public String toString() {
+        return "Vegetable{" + "id=" + id + ", name='" + name + '\'' + ", calories=" + calories + '}';
+    }
+}
